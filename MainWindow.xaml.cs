56e2fbc69beeb011f37a22dd258deed0efe1f5fa
Ashish Kumar Singh <singhashish@microsoft.com>
2024-03-05 09:59:38 +0530

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ABI.Windows.UI.ViewManagement;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.UI.ViewManagement;
+using UISettings = Windows.UI.ViewManagement.UISettings;
+
+namespace WPFAndWinAppSDK
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private UISettings _uiSettings;
+        private Color accent;
+        private Color ConvertToMediaColor(Windows.UI.Color winUIColor)
+        {
+            return Color.FromArgb(winUIColor.A, winUIColor.R, winUIColor.G, winUIColor.B);
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _uiSettings = new UISettings();
+            accent =  ConvertToMediaColor(_uiSettings.GetColorValue(UIColorType.Accent));
+        }
+    }
+}
